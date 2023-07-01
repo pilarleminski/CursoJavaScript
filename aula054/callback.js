fec07869fa958dce8b11404ff1bb3384be610bb6
Pilar Leminski Veiga <pilar.leminski@gmail.com>
2023-06-30 21:38:58 -0300

    nova aula

---
+++ aula054/callback.js
@@ -0,0 +1,61 @@
+// Criando o problema: eu não sei em que ordem as funções serão excutadas.
+
+function rand(min = 1000, max = 3000) {
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
+}
+
+
+function f1(callback) {
+    setTimeout(function () {
+        console.log('f1');
+        if(callback) callback();
+    }, rand());
+}
+    
+function f2(callback) {
+    setTimeout(function(){
+        console.log('f2');
+        if(callback) callback();
+    }, rand())
+}
+
+function f3(callback) {
+    setTimeout(function(){
+        console.log('f3');
+        if(callback) callback();
+    }, rand())
+}
+
+// f1();
+// f2();
+// f3();
+// console.log('Olá Mundo!');
+
+// Resolvendo o problema ♠ callback
+
+// f1(function(){
+//     f2(function(){
+//         f3(function(){
+//             console.log('Olá Mundo!');
+//         });
+//     });
+// });
+
+// Outra forma de Callback
+
+f1(f1Callback);
+
+function f1Callback(){
+    f2(f2Callback);
+}
+
+function f2Callback() {
+    f3(f3Callback);
+}
+
+function f3Callback() {
+    console.log('Olá Mundo!');
+}
+
+
